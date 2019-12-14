af864c818116a99fe2907b6b127e2cb568d3b979
Sergei Timushev <tsergeyrol08@gmail.com>
2019-12-15 00:18:02 +0300

    implement first variant user service

---
+++ src/services/user.service.ts
@@ -0,0 +1,44 @@
+import * as uuid from "uuid/v4";
+
+import User from "../models/user.model";
+
+const uniqueId: string = uuid();
+
+const UserCollection: User[] = [
+  {
+    id: uniqueId,
+    login: "Joe",
+    password: "qwerty1234",
+    age: 20,
+    isDeleted: false
+  }
+]
+
+class UserService {
+  private constructor() {}
+
+  private static _instance: UserService;
+
+  public static getInstance(): UserService {
+    return this._instance || (this._instance = new this());
+  };
+
+  public getUserById (id: string): User {
+    return UserCollection.find(item => item.id === id);
+  }
+
+  public getAllUsers (): User[] {
+    return UserCollection;
+  }
+
+  public addUser (user: User): void {
+    UserCollection.push(user);
+  }
+
+  public removeUserById (id: string): void {
+    const index = UserCollection.findIndex(item => item.id === id);
+    UserCollection.splice(index, 1);
+  }
+}
+
+export const userService = UserService.getInstance();

f3d08892564dcdf342f01e137113d97182d992f9
Sergei Timushev <tsergeyrol08@gmail.com>
2019-12-18 23:05:20 +0300

    tslint -> eslint

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,7 +1,7 @@
 export default interface User {
-  id: string,
-  login: string,
-  password: string,
-  age: number,
-  isDeleted: boolean
-}
+  id: string;
+  login: string;
+  password: string;
+  age: number;
+  isDeleted: boolean;
+};
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,44 +1,34 @@
-import * as uuid from "uuid/v4";
+import * as uuid from 'uuid/v4';
 
-import User from "../models/user.model";
+import User from '../models/user.model';
 
 const uniqueId: string = uuid();
 
 const UserCollection: User[] = [
-  {
-    id: uniqueId,
-    login: "Joe",
-    password: "qwerty1234",
-    age: 20,
-    isDeleted: false
-  }
-]
+    {
+        id: uniqueId,
+        login: 'Joe',
+        password: 'qwerty1234',
+        age: 20,
+        isDeleted: false
+    }
+];
 
-class UserService {
-  private constructor() {}
+export class UserService {
+    public getUserById(id: string): User {
+        return UserCollection.find(item => item.id === id);
+    }
 
-  private static _instance: UserService;
+    public getAllUsers(): User[] {
+        return UserCollection;
+    }
 
-  public static getInstance(): UserService {
-    return this._instance || (this._instance = new this());
-  };
+    public addUser(user: User): void {
+        UserCollection.push(user);
+    }
 
-  public getUserById (id: string): User {
-    return UserCollection.find(item => item.id === id);
-  }
-
-  public getAllUsers (): User[] {
-    return UserCollection;
-  }
-
-  public addUser (user: User): void {
-    UserCollection.push(user);
-  }
-
-  public removeUserById (id: string): void {
-    const index = UserCollection.findIndex(item => item.id === id);
-    UserCollection.splice(index, 1);
-  }
+    public removeUserById(id: string): void {
+        const index = UserCollection.findIndex(item => item.id === id);
+        UserCollection.splice(index, 1);
+    }
 }
-
-export const userService = UserService.getInstance();
